b9972da45658ceedbffc1a1399371f668975c4f0
Pedroporto87 <pedroporto_8@hotmail.com>
2023-04-27 16:21:58 -0300

    setup de rotas e alguma coisa de estilo

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/home'
+import { Sobre } from './pages/about'
+import { Contato } from './pages/contato.jsx'
 import App from './App.jsx'
-import './index.css'
+import './styles/main.scss'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <App />,
+    children: [
+      {
+        path:"/",
+        element: <Home />,
+      },
+      {
+        path:"/sobre",
+        element: <Sobre />,
+      },
+      {
+        path:"/contato",
+        element: <Contato />,
+      },
+    ],
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+<RouterProvider router={router} />
 )
