c194952a7883c802a5a8c682551a1455d4e00249
Pedroporto87 <pedroporto_8@hotmail.com>
2023-05-04 15:09:36 -0300

    foto e texto de apresentação feitos

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,11 @@
 import { BannerHome } from "../components/banner-home"
+import { HomeConteiner } from "../components/home-conteiner"
 
 const Home = () => {
   return (
     <main>
       <BannerHome />
+      <HomeConteiner />
     </main>
   )
 }
