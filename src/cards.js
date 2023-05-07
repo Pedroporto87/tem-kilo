0b70fd62616a04a3b630f1a77d20ded18d4cba94
Pedroporto87 <pedroporto_8@hotmail.com>
2023-05-06 23:38:41 -0300

    flatando form

--- src/cards.js
+++ src/cards.js
@@ -3,48 +3,48 @@ export const cards = [
         id: 1,
         name: 'Segunda',
         conteudo: 'Cozido',
-        icon: '../public/cards-img/cozido-modificado.jpg'
+        icon: '../cards-img/cozido-modificado.jpg'
     },
     {
         id: 2,
         name: 'Terça',
         conteudo: 'Cozinha Alemã',
-        icon: '../public/cards-img/german-food.jpg',
+        icon: '../cards-img/german-food.jpg',
     },
     {
         id: 3,
         name: 'Quarta',
         conteudo: 'Cozinha Árabe',
-        icon: '../public/cards-img/arabe-food.jpg',
+        icon: '../cards-img/arabe-food.jpg',
     },
     {
         id: 4,
         name: 'Quinta',
         conteudo: 'Cozinha Portuguesa',
-        icon: '../public/cards-img/bacalhau.jpg'
+        icon: '../cards-img/bacalhau.jpg'
     },
     {
         id: 5,
         name: 'Sexta',
         conteudo: 'Feijoada',
-        icon: '../public/cards-img/feijoada.png'
+        icon: '../cards-img/feijoada.png'
     },
     {
         id: 6,
         name: 'Sabado',
         conteudo: 'Frutos do Mar',
-        icon: '../public/cards-img/seafood.jpg'
+        icon: '../cards-img/seafood.jpg'
     },
     {
         id: 7,
         name: 'Domingo',
         conteudo: 'Comida Mineira',
-        icon: '../public/cards-img/feijoada.png'
+        icon: '../cards-img/feijoada.png'
     },
     {
         id: 8,
         name: 'Todos os Dias',
         conteudo: 'Japonês',
-        icon: '../public/cards-img/tk-3.jpg'
+        icon: '../cards-img/tk-3.jpg'
     },
 ]
