70a04b5e379bd27a0eef6ec7d4053e559d7e4731
Pedroporto87 <pedroporto_8@hotmail.com>
2023-05-17 20:14:20 -0300

    finalizando contato

--- src/pages/contato.jsx
+++ src/pages/contato.jsx
@@ -2,6 +2,7 @@ import { BannerContato } from "../components/banner-contato"
 import { ContatoConteiner } from "../components/contato-conteiner"
 
 
+
 export const Contato = () => {
   return (
     <>
