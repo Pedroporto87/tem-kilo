5a62a670027c45762cc1550c2d7c3186d1da8a53
Pedroporto87 <pedroporto_8@hotmail.com>
2023-05-27 12:15:07 -0300

    conectando ao emialjs

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,7 +1,9 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
+import emailjs from '@emailjs/browser'
 import '../styles/components/form.scss'
 
 export const Form = () => {
+    const [buttomText, setButtomText] = useState('Enviar')
     const [form, setForm] = useState({
         nome: '',
         email: '',
@@ -10,40 +12,66 @@ export const Form = () => {
         textbox: '',
  })
 
+const formRef = useRef()
+
 function handleChange(event) {
     setForm(event.target.value)
 }
 
+function handleSubmit(event) {
+    event.preventDefault();
+    emailjs
+      .sendForm(
+        'service_zb4g04r',
+        "template_y1mgcvt",
+        formRef.current,
+        "8p1FmUfF10_4rztXU"
+      )
+      .then(
+        (res) => {
+          setButtomText("Mensagem enviada com sucesso", () => {
+            setTimeout(() => {
+              setButtomText(false);
+            }, 3000);
+          });
+        },
+        (err) => {
+          setButtomText("Sua mensagem não foi enviada");
+        }
+      );
+  }
+
+
 
   return (
-    <form className='form'>
+    <form className='form' onSubmit={handleSubmit} ref={formRef}>
         <label htmlFor='nome'>Nome:
         <br />
-            <input name='nome' type='text' placeholder='Seu Nome' required onChange={handleChange} />
+            <input name={form.nome} type='text' placeholder='Seu Nome' required onChange={handleChange} />
         </label>
         <br />
         <label htmlFor='email'>E-mail:  
         <br />
-            <input name='email' type='email' placeholder='Seu E-mail' required onChange={handleChange} />
+            <input name={form.email} type='email' placeholder='Seu E-mail' required onChange={handleChange} />
         </label>
         <br />
         <label htmlFor='phone'>Telefone: 
         <br />
-            <input name='phone' type='tel' placeholder='Seu Telefone' required onChange={handleChange} />
+            <input name={form.telefone} type='tel' placeholder='Seu Telefone' required onChange={handleChange} />
         </label>
         <br />
         <label htmlFor='assunto'>Assunto:  
         <br />
-            <input name='assunto' type='text' placeholder='' onChange={handleChange}/>
+            <input name={form.assunto} type='text' placeholder='' onChange={handleChange}/>
         </label>
         <br />
         <label>Sua Mensagem: 
         <br />
-            <textarea name='textarea' type='text' style={{resize: 'none'}} placeholder='Como podemos lhe ajudar!' 
+            <textarea name={form.textbox} type='text' style={{resize: 'none'}} placeholder='Como podemos lhe ajudar!' 
             onChange={handleChange}
             required /></label>
         <br />
-        <button> Envie Aqui</button>
+        <button>{buttomText}</button>
     </form>
   )
 }
