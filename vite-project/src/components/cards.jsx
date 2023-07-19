37460e440d27e91f425d12fe0e51e9da567d2139
Pedroporto87 <pedroporto_8@hotmail.com>
2023-07-19 13:08:01 -0300

    ajustando logo para mobile

--- vite-project/src/components/cards.jsx
+++ vite-project/src/components/cards.jsx
@@ -3,13 +3,11 @@ import { cards } from '../cards'
 
 export const Cards = () => {
 
-    
-
   return (
     <>
         <section className='cards-title'>
             <h1>Todo dia tem comida boa!</h1>
-            <p>Além de uma farta variedade de pratos quentes e saladas do nosso buffet. Temos todo dia algum prato especial para você! </p>
+            <p>Além de uma farta variedade de pratos quentes e saladas do nosso buffet, temos todo dia algum prato especial para você! </p>
         </section>
         <section className='cards-conteiner'> 
             <section className='cards'>
