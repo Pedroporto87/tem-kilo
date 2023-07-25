64d48282cea783672966ab20aee58a908668e300
Pedroporto87 <pedroporto_8@hotmail.com>
2023-07-25 17:55:07 -0300

    inclusão do devPorto

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,20 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "./components/navbar"
 import { Footer } from "./components/footer"
+import './styles/components/devPorto.scss'
+
 
 function App() {
 
+  const devPorto = () => {
+    window.location.href='https://www.linkedin.com/in/pedro-porto-pp/'
+}
   return (
      <>
      <Navbar />
       <Outlet />
      <Footer />
+     <section onClick={devPorto} className="devPorto">DevPorto</section>
      </>
   )
 }
